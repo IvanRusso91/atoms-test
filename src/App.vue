3d8d00e11980433c4f15e121796957df9fcc3407
IvanRusso91 <iv.russo91@gmail.com>
2023-01-17 13:08:08 +0100

    progetto-test

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,114 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+      <header>
+        <!-- Navbar -->
+        <div class="container box-nav d-flex">
+          <!-- logo -->
+          <div class="container">
+            <img src="./assets/img/logo-3.svg" alt="logo-3">
+          </div>
+          <!-- /logo -->
+
+          <!-- btn navbar -->
+          <div class="container d-flex btn-nav">
+            <nav>
+              <ul>
+                <li>
+                  <a href="#">Works</a>
+                </li>
+                <li>
+                  <a href="#">Services</a>
+                </li>
+                <li>
+                  <a href="#">Why us</a>
+                </li>
+                <li>
+                  <a class="last" href="#">Let's talk</a>
+                </li>
+                <li>
+                  <div class="box-icon">
+                    <div class="rec-copy"></div>
+                    <div class="rec-copy"></div>
+                    <small>menu</small>               
+                  </div>
+                </li>
+              </ul>
+            </nav>
+
+          </div>
+          <!-- /btn navbar -->
+
+        </div>
+      </header>
+
+      <section>
+        <JumboComp />
+      </section>
+      <!-- /Navbar -->
+
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import JumboComp from '@/components/JumboComp';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    JumboComp,
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+@import './assets/style/_global.scss';
+@import './assets/style/_vars.scss';
+
+.box-nav{
+  margin-top: 30px;
+  justify-content: space-between;
+  .btn-nav{
+    justify-content: end;
+    li{
+      margin-top: 10px;
+      display: inline-block;
+      text-transform: uppercase;
+      text-align: center;
+      a{  
+        font-weight: bold;
+        margin-right: 22px;
+        padding: 15px 16px 15px 16px;
+        &:hover{
+          background-color: $fourth-color;
+          color: $secondary-color;
+          border-radius: 30px;
+        }
+      }
+      .last{
+        background-color: $fourth-color;
+        color: $secondary-color;
+        border-radius: 30px;
+      }
+      .box-icon{
+        .rec-copy{
+          height: 2px;
+          width: 24px;
+          border-radius: 1px;
+          background-color: #484A54;
+          &:nth-child(2){
+            margin-top: 2px;
+            margin-bottom: -10px;
+          }
+        }
+        small{
+          font-size: 0.5em;
+          font-weight: bold;
+        }
+      }
+    }
+  }
 }
+
 </style>
---
+++ src/components/JumboComp.vue
@@ -0,0 +1,109 @@
+<template>
+  <div>
+    <div class="container jumbo-box ">
+
+      <!-- titolo + bottone -->
+      <div class="container perfomcance-jumbo">
+        <h1>
+          Performace without compromises
+        </h1>
+        <p>
+          How we helped Blumarine to conquer new business
+        </p>
+        <button class="btn">
+          Discover
+        </button>
+
+        <!-- blumarine -->
+        <div class="jumbo-blumarine d-flex">
+          <h4>
+            Blumarine
+          </h4>
+          <small>
+            Front end, Design UX/UI
+          </small>
+        </div>
+        <!-- /blumarine -->
+   
+      </div>
+      <!-- /titolo + bottone -->
+      
+      <!-- jumbotron immagine -->
+      <div class="jumbo-img"></div>
+      <!-- /jumbotron immagine -->
+
+      <!-- sfera -->
+      <div class="sfera"></div>  
+      <!-- /sfera -->
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'JumboComp',
+}
+</script>
+
+<style lang= "scss" scoped>
+
+@import '../assets/style/_vars.scss';
+@import '../assets/style/_utilities.scss';
+
+
+.jumbo-box{
+  position: relative;
+  .perfomcance-jumbo{
+    margin-top: 176px;
+    h1{
+      width: 814px;
+      font-size: 72px;
+    }
+    p{
+      color: $fifth-color;
+      width: 329px;
+      font-size: 1.2rem;
+    }
+    .btn{
+      width: 121px;
+      transition: 0.5s;
+      @include btn-1;
+      &:hover{
+        background-color: $fourth-color;
+        color: $secondary-color;
+      }
+    }
+    .jumbo-blumarine{
+      width: 400px;
+      text-align: right;
+      flex-direction: column;
+      margin-top: 70px;
+      small{
+        color: $fifth-color;
+      }
+    }
+  }
+  .jumbo-img{
+    position: absolute;
+    z-index: -1;
+    height: 566px;
+    width: 755px;
+    background-image: url('../assets/img/Blauer_Detroit_D5_612.jpg');
+    top:-120px;
+    right: 135px;
+  }
+  .sfera{
+    position: absolute;
+    z-index: -2;
+    top: -160px;
+    right: -40px;
+    width: 880px;
+    height: 880px;
+    border-radius: 50%;
+    background-color: $sixth-color;
+  }
+
+}
+
+</style>
